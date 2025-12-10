5be3afbb953b2a5bce8e94e5150fa2106b064306
James Njenga <99135524+KINYENJE@users.noreply.github.com>
2025-12-10 16:15:46 +0300

    fix typo in Forgot Password page message for improved clarity

--- app/forgot-password/page.jsx
+++ app/forgot-password/page.jsx
@@ -52,7 +52,7 @@ const ForgotPassword = () => {
         
         <form onSubmit={handleSubmit} className={`space-y-6 w-full ${fontSyne.className}`}>
           <p className='text-dsectext dark:text-dsectext text-center'>
-            Enter your email address and we'll send you a link to reset your password.
+            Enter your email address and we&apos;ll send you a link to reset your password.
           </p>
           
           <input 
